f723179d373203cc4b9b6f1d2c496f09aea603b7
Kenny Cheng <you@example.com>
2021-06-23 15:59:13 +0800

    adding width and height settings.

--- app.js
+++ app.js
@@ -14,13 +14,16 @@ app.get('/:u', async (req, res) => {
   console.log(`${req.params.u} url is ${url}`);
   if (fs.existsSync(img)) {
     return res.sendFile(img);
-  }
-
-
+  })
 
   const browser = await puppeteer.launch({args:['--no-sandbox']});
   //const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium', args:['--no-sandbox']});
   const page = await browser.newPage();
+  await page.setViewport({
+    width: parseInt(req.query.w) || 1024,
+    height: parseInt(req.query.h) || 600,
+    deviceScaleFactor: 1,
+  });
   await page.goto(url);
   await page.screenshot({path: img});
   await browser.close();
