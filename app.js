94d85be45aeae4a3b2f7327795d6b6a956912454
Kenny Cheng <kennycheng@comp.hkbu.edu.hk>
2023-07-07 16:32:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const fs = require('fs')
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-core')
 const { createHash } = require('crypto')
 const app = express()
 const port = process.env.PORT || 5000
